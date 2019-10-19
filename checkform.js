42f112c72a2312fd734c5695e48c8311da405244
Kirill <41589091+Traineratwot@users.noreply.github.com>
2019-10-19 23:56:45 +0300

    bug-fix

--- checkform.js
+++ checkform.js
@@ -9,34 +9,34 @@ $rule.phone = { 'regexp': /\(?\+[0-9]{1,3}\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-
 jQuery(document).ready(function($) {
     $("form button,form input[type='button'],input[type='submit']").on('click',
         function(t) {
-            var test = true;
+            var test = [];
             var selecter
-            if ($(this)[0].form) {
-                selecter = $(this)[0].form
-            } else if ($(this).attr('form')) {
+            if ($(this).attr('form')) {
                 selecter = $('form[name=' + $(this).attr('form') + ']')
+            } else if ($(this)[0].form) {
+                selecter = $(this)[0].form
             } else {
                 console.warn('где форма?')
                 return true;
             }
-            $(selecter).find('input[required]').each(function(index, element) {
+            $(selecter).find('input[required],textarea[required]').each(function(index, element) {
                 var x = $(element).val()
                 if (x === false || x == '' || x == 'undefind') {
                     // console.log('X')
-                    test = false
                     var tooltip = $('<span>', {
                         css: {
                             'width': 'fit - content',
                             'z-index': 9999,
-                            'background': '#b5b5b5bd',
+                            'background': '#b5b5b5',
                             'color': '#20272F',
-                            'padding': '5px',
-                            'border-radius': '2px'
+                            'padding': '2px 8px',
+                            'position': 'absolute',
+                            'border-radius': '10px'
                         },
-                        html: '<i class="fas fa-exclamation-circle" style="color:gold;"></i> Не заполнено поле'
+                        html: '<i class="fas fa-exclamation-circle" style="color:gold;"></i>Поле не заполнено '
                     }).appendTo('body');
                     $(tooltip).offset({
-                        top: $(element).offset().top - $(element).height() - 7,
+                        top: $(element).offset().top - $(tooltip).height() - 5,
                         left: $(element).offset().left
                     })
                     $(tooltip).fadeOut(0).fadeIn(500)
@@ -46,31 +46,33 @@ jQuery(document).ready(function($) {
                             $(tooltip).remove()
                         }, 500);
                     }, 2500);
-
+                    test.push(1)
                 } else {
                     type = $(element).attr('data-type') ? $(element).attr('data-type') : $(element).attr('type')
                     pattern = $(element).attr('data-pattern') ? $(element).attr('data-pattern') : $(element).attr('pattern')
                     if ((type && $rule[type]) || pattern) {
                         var str = $(element).val()
-                        test = false
+                        var er = false
                         if (type) {
                             var sr = $rule[type].regexp.exec(str)
                             if (sr !== null) {
-                                test = true
+                                er = true
+                            } else {
+                                test.push(1)
                             }
                             if (!$rule[type].name) {
                                 $rule[type].name = type
                             }
                         } else {
-                            pattern = new RegExp(pattern, i)
+                            pattern = new RegExp(pattern, "i")
                             var sr = pattern.exec(str)
                             if (sr !== null) {
-                                test = true
+                                er = true
+                            } else {
+                                test.push(1)
                             }
                         }
-
-
-                        if (!test) {
+                        if (!er) {
                             var name = $rule[type].name ? $rule[type].name : 'то что нужно'
                             var tooltip = $('<span>', {
                                 css: {
@@ -84,7 +86,7 @@ jQuery(document).ready(function($) {
                                 html: '<i class="fas fa-exclamation-circle" style="color:gold;"></i> это не похоже на ' + name
                             }).appendTo('body');
                             $(tooltip).offset({
-                                top: $(element).offset().top - $(element).height() - 7,
+                                top: $(element).offset().top - $(tooltip).height() - 5,
                                 left: $(element).offset().left
                             })
                             $(tooltip).fadeOut(0).fadeIn(500)
@@ -98,9 +100,9 @@ jQuery(document).ready(function($) {
                     }
                 }
             });
-            if (test) {
+            if (test.indexOf(1) == -1) {
                 eval($(t.currentTarget).attr('data-click'))
             }
         }
     );
-})
+});
