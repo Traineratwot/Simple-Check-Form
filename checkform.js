2de67e21d4b728069247f935154385b4b08a0142
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-03-27 15:47:42 +0300

    1

--- checkform.js
+++ checkform.js
@@ -1,10 +1,10 @@
-var $rule = {};
-$rule.number = { 'regexp': /\d*/, 'name': 'число' };
-$rule.date = { 'regexp': /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/, 'name': 'дата' };
-$rule.float = { 'regexp': /\-?\d+(\.\d{0,})?/, 'name': 'дробное число' };
-$rule.name = { 'regexp': /^[а-яА-ЯёЁa-zA-Z\s]+$/i, 'name': 'имя' };
-$rule.email = { 'regexp': /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/i, 'name': 'Почту' };
-$rule.phone = { 'regexp': /[0-9()\+\-\_\=]+/i, 'name': 'телефон' };
+var $rule = {}
+$rule.number = { 'regexp': /\d*/, 'name': 'число' }
+$rule.date = { 'regexp': /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/, 'name': 'дата' }
+$rule.float = { 'regexp': /\-?\d+(\.\d{0,})?/, 'name': 'дробное число' }
+$rule.name = { 'regexp': /^[а-яА-ЯёЁa-zA-Z\s]+$/i, 'name': 'имя' }
+$rule.email = { 'regexp': /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/i, 'name': 'Почту' }
+$rule.phone = { 'regexp': /[0-9()\+\-\_\=]+/i, 'name': 'телефон' }
 var $style = {
 	'width': 'fit - content',
 	'z-index': 9999,
@@ -14,49 +14,49 @@ var $style = {
 	'position': 'absolute',
 	'border-radius': '10px'
 }
-jQuery(document).ready(function ($) {
-	$("form button,form input[type=button],input[type=submit],input[form],button[form]").on('click',
-		function (t) {
-			var test = [];
-			var selecter;
+function checkform() {
+	$('form button[data-submit],form input[type=button],input[type=submit],input[form],button[form]').on('click',
+		function (event) {
+			var test = []
+			var selecter
 			if ($(this).attr('form')) {
 				selecter = $('form[name=' + $(this).attr('form') + ']')
 			} else if ($(this)[0].form) {
-				selecter = $(this)[0].form;
+				selecter = $(this)[0].form
 			} else {
 				console.warn('где форма?')
-				return true;
+				return true
 			}
 			$(selecter).find('input,textarea').each(function (index, element) {
-				var x = $(element).val();
-				type = $(element).attr('data-type') ? $(element).attr('data-type') : $(element).attr('type');
-				pattern = $(element).attr('data-pattern') ? $(element).attr('data-pattern') : $(element).attr('pattern');
+				var x = $(element).val()
+				type = $(element).attr('data-type') ? $(element).attr('data-type') : $(element).attr('type')
+				pattern = $(element).attr('data-pattern') ? $(element).attr('data-pattern') : $(element).attr('pattern')
 				if (x !== false && x != '' && x != 'undefind') {
 					if ((type && $rule[type]) || pattern) {
-						var str = $(element).val();
-						var er = false;
+						var str = $(element).val()
+						var er = false
 						if (type) {
 							var sr = $rule[type].regexp.exec(str)
 							if (sr !== null) {
-								er = true;
+								er = true
 							} else {
-								test.push(1);
+								test.push(1)
 							}
 							if (!$rule[type].name) {
-								$rule[type].name = type;
+								$rule[type].name = type
 							}
 						} else {
-							pattern = new RegExp(pattern, "i");
-							var sr = pattern.exec(str);
+							pattern = new RegExp(pattern, 'i')
+							var sr = pattern.exec(str)
 							if (sr !== null) {
-								er = true;
+								er = true
 							} else {
-								test.push(1);
+								test.push(1)
 							}
 						}
 						if (!er) {
-							var name = $rule[type].name ? $rule[type].name : 'то что нужно';
-							attention(element, '🛑 это не похоже на ' + name);
+							var name = $rule[type].name ? $rule[type].name : 'то что нужно'
+							attention(element, '🛑 это не похоже на ' + name)
 						}
 					}
 				}
@@ -64,66 +64,68 @@ jQuery(document).ready(function ($) {
 					switch (type) {
 						case 'checkbox':
 							if (!$(element).prop('checked')) {
-								attention(element, '🛑 вы должны отметить это');
-								test.push(1);
+								attention(element, '🛑 вы должны отметить это')
+								test.push(1)
 							}
-							break;
+							break
 						default:
 							if (x === false || x == '' || x == 'undefind') {
 								// console.log('X')
-								attention(element, '🛑 Поле не заполнено');
-								test.push(1);
+								attention(element, '🛑 Поле не заполнено')
+								test.push(1)
 							}
-							break;
+							break
 					}
 				}
 
-			});
+			})
 			if ($(selecter).attr('data-required')) {
-				var Vlist = false;
+				var Vlist = false
 				var reqlist = $(selecter).attr('data-required').split(',')
 				reqlist.forEach(elemselec => {
 					var x = $(elemselec).val()
 					if (x !== false && x != '' && x != 'undefind') {
 						Vlist = true
 					}
-				});
+				})
 				if (!Vlist) {
 					reqlist.forEach(elemselec => {
 						var x = $(elemselec).val()
-						var type = $(elemselec).attr('data-type') ? $(elemselec).attr('data-type') : $(elemselec).attr('type');
+						var type = $(elemselec).attr('data-type') ? $(elemselec).attr('data-type') : $(elemselec).attr('type')
 						switch (type) {
 							case 'checkbox':
 								if (!$(elemselec).prop('checked')) {
-									attention(elemselec, '🛑 Что-то из этого должно быть отмеченно');
-									test.push(1);
+									attention(elemselec, '🛑 Что-то из этого должно быть отмеченно')
+									test.push(1)
 								}
-								break;
+								break
 							default:
 								if (x === false || x == '' || x == 'undefind') {
 									// console.log('X')
-									attention(elemselec, '🛑 Что-то из этого должно быть заполнено');
-									test.push(1);
+									attention(elemselec, '🛑 Что-то из этого должно быть заполнено')
+									test.push(1)
 								}
-								break;
+								break
 						}
-					});
+					})
 
 				}
 			}
 			if (test.indexOf(1) == -1) {
 				if ($(t.currentTarget).attr('data-click')) {
 					eval($(t.currentTarget).attr('data-click'))
+					event.preventDefault()
 					return false
 				} else {
 					return true
 				}
 			} else {
+				event.preventDefault()
 				return false
 			}
 		}
-	);
-});
+	)
+}
 
 function attention(element, text) {
 	var tooltip = $('<span>', {
@@ -142,3 +144,6 @@ function attention(element, text) {
 		}, 500);
 	}, 2500);
 }
+
+
+jQuery(document).ready(checkform())
